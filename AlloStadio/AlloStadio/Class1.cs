92648578e70fb117e051c1dd6744973ed0a24e44
SStarSir <43829251+SStarSir@users.noreply.github.com>
2020-05-22 21:09:27 +0100

    Add files via upload

---
+++ AlloStadio/AlloStadio/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlloStadio
+{
+    class Team 
+    {
+
+        string teamName;
+        int teamAttack;
+        int teamMidfield;
+        int teamDefense;
+        //int clock = 0;
+
+
+
+
+    }
+}
---
+++ AlloStadio/AlloStadio/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Globalization;
+
+namespace Game
+{
+    static class Madlib
+    {
+        //declare variables
+        static string[] Squadre = new string[] { "creature", "luminous" };
+        static string[] Prompts = new string[] { " squadra di casa", " squadra in trasferta", "adjective", "adjective", "occupation", "occupation", "occupation", "adjective", "noun", "noun" };
+        static string Story;
+        static string GameTitle;
+
+
+
+        
+
+
+        public static void Run()
+        {
+
+
+
+
+            Header();
+            AskQuestions();
+            ReadStory();
+            End();
+        }
+
+        static void Header()
+        {
+
+            //write header and instructions
+
+            Console.Title = "ASCII Art";
+            string title = @"
+
+                    
+          _      _      ____     _____ _______       _____ _____ ____  
+    /\   | |    | |    / __ \   / ____|__   __|/\   |  __ \_   _/ __ \ 
+   /  \  | |    | |   | |  | | | (___    | |  /  \  | |  | || || |  | |
+  / /\ \ | |    | |   | |  | |  \___ \   | | / /\ \ | |  | || || |  | |
+ / ____ \| |____| |___| |__| |  ____) |  | |/ ____ \| |__| || || |__| |
+/_/    \_\______|______\____/  |_____/   |_/_/    \_\_____/_____\____/ 
+                                                                       
+
+
+
+              ";
+
+            Console.WriteLine(title);
+
+            Console.WriteLine("-------");
+            Console.WriteLine("Un programma di Roy & Spuk!");
+            Console.WriteLine("Presented by IFCATS");
+            Console.WriteLine("-------");
+
+
+
+        }
+
+        static void AskQuestions()
+
+        {
+
+            //ask player to enter words
+
+            //ask player to enter words
+            for (int i = 0; i < Squadre.Length; i++)
+            {
+                Console.Write("Inserisci la" + Prompts[i] + ": ");
+                Squadre[i] = Console.ReadLine();
+            }
+
+            //clear the console window
+            Console.Clear();
+
+
+        }
+
+
+        static void ReadStory()
+
+        {
+            //write out story
+
+
+            //Concatenate strings to make a title
+            GameTitle = "La partita di oggi: " + Squadre[0] + " contro " + Squadre[1];
+
+            //So we can capitalize the words in our title
+            TextInfo TitleCase = new CultureInfo("en-US", false).TextInfo;
+            GameTitle = TitleCase.ToTitleCase(GameTitle);
+
+            //Write the title to the console window
+            Console.WriteLine(GameTitle);
+            Console.WriteLine(" ");
+
+            //change the title of the application window
+            Console.Title = GameTitle;
+
+            Story = "Buonasera da Fabio Caressa e Beppe Bergomi. Tutto é pronto per il calcio di inizio. \nOggi si affronteranno {0} e {1}. \nPartiti! {0} batte il calcio di inizio.";
+            Console.WriteLine(Story, Squadre[0], Squadre[1]);
+
+
+            int homescore = 0;
+            int awayscore = 0;
+            
+            for (int clock = 1; clock < 91; clock++)
+
+            {
+
+                
+                System.Threading.Thread.Sleep(1000);
+
+                Random r = new Random();
+                int rInt = r.Next(0, 100); //for ints
+
+                if (rInt < 10)
+                {
+                    Console.WriteLine("GOAL!");
+                    homescore = homescore + 1;
+                }
+
+                else if (rInt > 90)
+
+                {
+                    Console.WriteLine("GOAL!");
+                    awayscore = awayscore + 1;
+                }
+
+                else
+
+                {
+                    Console.WriteLine("Non succede un cazzo al minuto " + clock);
+                }
+            }
+
+                
+
+            
+
+            Console.WriteLine("Triplice fischio. Finisce 0 a 0, una partita povera di emozioni");
+
+        }
+
+        static void End()
+        {
+            //keep window open and prompt for exit
+            Console.WriteLine("Press enter to exit");
+            Console.ReadKey();
+        }
+
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Madlib.Run();
+
+        }
+    }
+}
